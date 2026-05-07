c4f38d3f059c3ba44f9e5d83eaf4ce1406aa1fa4
kiran-yogesh <161602355+kiran-yogesh@users.noreply.github.com>
2026-05-07 12:26:54 +0530

    Update auth.api.js

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://mern-backend-tcx2.onrender.com/api/auth';
 
 const api = axios.create({
   baseURL: API_URL,
